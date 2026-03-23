8f3c508031be312c8f60932b3ad75688fbebc81b
dushes <dushes.nadym@gmail.com>
2026-03-23 18:14:18 +0800

    Add status endpoint to check env variables

---
+++ app/api/status/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  return NextResponse.json({
+    TELEGRAM_BOT_ID: !!process.env.NEXT_PUBLIC_TELEGRAM_BOT_ID,
+    BASE_URL: !!process.env.NEXT_PUBLIC_BASE_URL,
+    BLOB_TOKEN: !!process.env.BLOB_READ_WRITE_TOKEN,
+    MESSAGE_LIMIT: !!process.env.DEFAULT_MESSAGE_LIMIT,
+  });
+}
